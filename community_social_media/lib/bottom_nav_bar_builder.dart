6ba3f4415cc8207a396df80e75f3324f2decf591
alicimen0614 <alicimen0614@gmail.com>
2024-05-18 09:26:18 +0300

    first commit

---
+++ community_social_media/lib/bottom_nav_bar_builder.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBarBuilder extends StatelessWidget {
+  const BottomNavBarBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
