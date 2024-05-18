53437d3d39ca7366c4cdcddc252fced754161360
alicimen0614 <alicimen0614@gmail.com>
2024-05-18 10:30:58 +0300

    bottom navigation bar added and auth screen template placed

--- community_social_media/lib/bottom_nav_bar_builder.dart
+++ community_social_media/lib/bottom_nav_bar_builder.dart
@@ -1,12 +1,49 @@
+import 'package:community_social_media/auth_screen/auth_screen.dart';
+import 'package:community_social_media/screens/events_screen/events_screen.dart';
+import 'package:community_social_media/screens/user_screen/profile_screen.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavBarBuilder extends StatelessWidget {
+import 'screens/explore_screen/explore_screen.dart';
+
+class BottomNavBarBuilder extends StatefulWidget {
   const BottomNavBarBuilder({super.key});
 
   @override
+  State<BottomNavBarBuilder> createState() => _BottomNavBarBuilderState();
+}
+
+class _BottomNavBarBuilderState extends State<BottomNavBarBuilder> {
+  int currentIndex = 0;
+  final List<Widget> screens = <Widget>[
+    const ExploreScreen(),
+    const EventsScreen(),
+    const AuthScreen()
+  ];
+
+  final List<Widget> items = [
+    const NavigationDestination(
+        icon: Icon(
+          Icons.explore_rounded,
+        ),
+        label: ""),
+    const NavigationDestination(icon: Icon(Icons.event), label: ""),
+    const NavigationDestination(icon: Icon(Icons.person), label: ""),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("merhaba"),
-    );
+    return Scaffold(
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.miniCenterDocked,
+        body: screens[currentIndex],
+        bottomNavigationBar: NavigationBar(
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+          destinations: items,
+          selectedIndex: currentIndex,
+          onDestinationSelected: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ));
   }
 }
